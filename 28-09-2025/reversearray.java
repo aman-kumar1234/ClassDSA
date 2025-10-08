5245ce84ba0763650a084a68c8bdb13d52b94b7a
aman-kumar1234 <aman.24scse1330015@galgotiasuniversity.ac.in>
2025-10-08 11:19:55 +0530

    Update and rename q2.java to reversearray.java

--- 28-09-2025/reversearray.java
+++ 28-09-2025/reversearray.java
@@ -1,6 +1,6 @@
 // Q2. Write a program to reverse an array.
 
-class Q2 {
+class Main {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int n = arr.length;
@@ -14,3 +14,4 @@ class Q2 {
         for (int x : arr) System.out.print(x + " ");
     }
 }
+
