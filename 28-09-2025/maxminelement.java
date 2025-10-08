56c22368df98dcc9883ad74467c08273aaad96c0
aman-kumar1234 <aman.24scse1330015@galgotiasuniversity.ac.in>
2025-10-08 11:22:41 +0530

    Update and rename q4.java to maxminelement.java

--- 28-09-2025/maxminelement.java
+++ 28-09-2025/maxminelement.java
@@ -1,6 +1,6 @@
 // Q4. Write a program to find the maximum and minimum element in an array.
 
-class Q4 {
+class Main {
     public static void main(String[] args) {
         int[] arr = {10, 3, 45, 2, 9};
         int max = arr[0], min = arr[0];
@@ -13,3 +13,4 @@ class Q4 {
         System.out.println("Max = " + max + ", Min = " + min);
     }
 }
+
